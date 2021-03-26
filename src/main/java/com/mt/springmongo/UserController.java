58c5fef9730b4f63cce8987692329e989ba32a31
srikanthweb <ganjisrikanthweb@gmail.com>
2021-03-26 20:55:33 +0530

    all

--- src/main/java/com/mt/springmongo/UserController.java
+++ src/main/java/com/mt/springmongo/UserController.java
@@ -1,7 +1,5 @@
 package com.mt.springmongo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UserController {
     private final UserRepository userRepository;
-    
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     public UserController(final UserRepository userRepository) {
@@ -28,7 +24,6 @@ public class UserController {
                        @RequestParam("lastName") String lastName,
                        @RequestParam("email") String email) {
 
-    	logger.info("Creating user name: "+firstName);
         User user = new User(firstName, lastName, email);
         userRepository.save(user);
 
